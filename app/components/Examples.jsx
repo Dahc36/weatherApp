2a043232044d67ae666634e972bdeb3adbd02d55
David Alberto Hans Castillo <dhansc@bch.bancodechile.cl>
2017-10-31 17:41:08 -0300

    ADD: style main & examples

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,27 @@
 let React = require('react');
+let {Link} = require('react-router');
 
 let Examples = (props) => {
 	return (
 		<div>
-			<h3>Examples</h3>
-			<p>Welcome to the examples page.</p>
+			<h1 className="text-center">Examples</h1>
+			<p>Here are a few examples you can try out.</p>
+			<ol>
+				<li>
+					<Link to="/?location=Philadelphia">Philadelphia</Link>
+				</li>
+				<li>
+					<Link to="/?location=Santiago">Santiago</Link>
+				</li><li>
+					<Link to="/?location=London">London</Link>
+				</li><li>
+					<Link to="/?location=Lima">Lima</Link>
+				</li><li>
+					<Link to="/?location=Seoul">Seoul</Link>
+				</li><li>
+					<Link to="/?location=La Florida">La Florida</Link>
+				</li>
+			</ol>
 		</div>
 	);
 };
